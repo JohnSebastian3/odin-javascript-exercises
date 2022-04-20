1370466b61bd07b221ab3fc75c639e9fbfbe5066
Sebastian <sebastianbiochem@gmail.com>
2022-04-20 12:55:24 -0400

    Add solution to exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+  for(let i = 0; i < args.length; i++) {
+    if(arr.includes(args[i])) {
+      arr.splice(arr.indexOf(args[i]), 1);
+    }
+  }
+  return arr;
 };
+
+console.log(removeFromArray([1,2,3,4,5], 3)); // Should give [1,2,4,5]
+console.log(removeFromArray([1, 2, 3, 4], 3, 2)) // Should give [1,4];
 
 // Do not edit below this line
 module.exports = removeFromArray;
