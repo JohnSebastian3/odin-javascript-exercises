b787e27f45fae2137c7a19df3a1cc44f4df39a0e
Sebastian <sebastianbiochem@gmail.com>
2022-04-20 18:46:05 -0400

    Add solution for exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if(year % 100 === 0) {
+    if(year % 400 === 0) {
+      return true;
+    }
+    return false;
+  } else if(year % 4 === 0) {
+    return true;
+  }
+  return false;
 };
+
+console.log(leapYears(2000)); // True
+console.log(leapYears(1985)); // False
+console.log(leapYears(2004)); // True
+console.log(leapYears(1900)); // False
 
 // Do not edit below this line
 module.exports = leapYears;
