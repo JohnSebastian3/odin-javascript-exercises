33b5788c046c5c8bd45c6b4554bfd67b61849ab2
Sebastian <sebastianbiochem@gmail.com>
2022-04-20 18:54:02 -0400

    Add solution for exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+  return +((ftemp - 32) * 5/9).toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+  return +((ctemp * 9/5) + 32).toFixed(1);
 };
+
+console.log(ftoc(32));
+console.log(ctof(0));
 
 // Do not edit below this line
 module.exports = {
