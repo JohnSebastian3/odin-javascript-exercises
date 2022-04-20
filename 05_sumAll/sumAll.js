d8e190bf2ba505e6444fca0111d01ab7d6502c83
Sebastian <sebastianbiochem@gmail.com>
2022-04-20 18:32:29 -0400

    Add solution to exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+  if(start < 0 || end < 0 || typeof start !== 'number' || typeof end !== 'number') {
+    return 'ERROR';
+  }
+
+  let sum = 0;
+  if(start < end) {
+    for(let i = start; i <= end; i++) {
+      sum += i;
+    }
+  } else if(start > end) {
+    for(let i = start; i >= end; i--) {
+      sum += i;
+    }
+  } else {
+    return start;
+  }
+  return sum;
 };
-
+console.log(sumAll(1,4));
+console.log(sumAll(123, 1));
 // Do not edit below this line
 module.exports = sumAll;
