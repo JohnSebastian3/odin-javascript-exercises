4b2fea9bef727e002588966044e383fbac53199e
Sebastian <sebastianbiochem@gmail.com>
2022-04-22 19:15:53 -0400

    Add solution for exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.toLowerCase().replace(/[^a-z]/g, "")
+  let reversed = str.split('').reverse().join('');
+  return str === reversed;
 };
+
+console.log(palindromes('racecar!'));
+console.log(palindromes('Racecar!'));
+console.log(palindromes('A car, a man, a maraca.'));
+console.log(palindromes('Animal loots foliated detail of stool lamina.'));
 
 // Do not edit below this line
 module.exports = palindromes;
