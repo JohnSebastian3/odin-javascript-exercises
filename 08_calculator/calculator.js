a9c6f93de322d010444468d78928602086500b9d
Sebastian <sebastianbiochem@gmail.com>
2022-04-22 18:57:31 -0400

    Add solution for exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if(arr.length > 0) {
+    return arr.reduce((acc, curr) => {
+      return acc += curr;
+    })
+  }
+
+  return 0;
+
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  if(arr.length > 0) {
+    return arr.reduce((acc, curr) => {
+      return acc *= curr;
+    }, 1)
+  }
 
+  return 0;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if(x === 0) {
+    return 1;
+  }
+  return x * factorial(x - 1);
 };
 
 // Do not edit below this line
