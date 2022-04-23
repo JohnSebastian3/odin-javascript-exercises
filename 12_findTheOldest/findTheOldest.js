8173716c8ea8890ad826f13c432de6b82f534b6f
Sebastian <sebastianbiochem@gmail.com>
2022-04-23 10:11:51 -0400

    Add solution for exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,41 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(objArr) {
+  objArr.sort((first, second) => {
+    if(!first.yearOfDeath) {
+      const currentYear = new Date().getFullYear();
+      const firstLived = currentYear - first.yearOfBirth;
+      const secondLived = second.yearOfDeath - second.yearOfBirth;
+      return firstLived - secondLived;
+    } else if (!second.yearOfDeath) {
+      const currentYear = new Date().getFullYear();
+      const secondLived = currentYear - second.yearOfBirth;
+      const firstLived = first.yearOfDeath - first.yearOfBirth;
+      return firstLived - secondLived;
+    } else {
+ 
+      const firstLived = first.yearOfDeath - first.yearOfBirth;
+      const secondLived = second.yearOfDeath - second.yearOfBirth;
+      return firstLived - secondLived;
+    }
+  });
+  return objArr[objArr.length - 1];
 };
+
+console.log(findTheOldest([
+  {
+    name: "Carly",
+    yearOfBirth: 1066,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]))
 
 // Do not edit below this line
 module.exports = findTheOldest;
