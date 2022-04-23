db8e96378eacd3275e3867f244c50dfc822635d0
Sebastian <sebastianbiochem@gmail.com>
2022-04-23 09:37:10 -0400

    Add solution to exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+  const titleArr = [];
+  arr.forEach(elem => {
+    titleArr.push(elem.title);
+  })
+  return titleArr;
 };
+
+console.log(getTheTitles([
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]))
 
 // Do not edit below this line
 module.exports = getTheTitles;
